b7928ca514205f8a74986647c1a0993aa67d278d
Leonardo Luz <leoluz@users.noreply.github.com>
2016-10-13 15:14:04 -0400

    Integrate with plantuml library

--- src/main/groovy/com/guml/domain/Diagram.groovy
+++ src/main/groovy/com/guml/domain/Diagram.groovy
@@ -1,5 +1,17 @@
 package com.guml.domain
 
+import net.sourceforge.plantuml.SourceStringReader
+
 class Diagram {
     def id, name
+    String dsl
+
+    def buildImage() {
+        if (dsl) {
+            OutputStream os = new ByteArrayOutputStream()
+            SourceStringReader reader = new SourceStringReader(dsl)
+            reader.generateImage(os)
+            os.toByteArray()
+        }
+    }
 }
