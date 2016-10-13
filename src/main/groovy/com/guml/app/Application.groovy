6eec4dec3ebb478372e862d43324b49a0fee4091
Leonardo Luz Almeida <leonardo.la@gmail.com>
2016-10-12 20:51:16 -0400

    Initial commit

---
+++ src/main/groovy/com/guml/app/Application.groovy
@@ -0,0 +1,13 @@
+package com.guml.app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+    static void main(String[] args) {
+        SpringApplication.run Application, args
+    }
+
+}
