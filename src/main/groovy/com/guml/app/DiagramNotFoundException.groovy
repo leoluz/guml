d24f71ce8f890b4594f090e1d30e1c344e01017d
Leonardo Luz Almeida <leo.almeida@appdirect.com>
2016-10-14 11:00:24 -0400

    Add exception handler for diagram not found

---
+++ src/main/groovy/com/guml/app/DiagramNotFoundException.groovy
@@ -0,0 +1,7 @@
+package com.guml.app
+
+class DiagramNotFoundException extends RuntimeException {
+    public DiagramNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/groovy/com/guml/infra/GitRepository.groovy
+++ src/main/groovy/com/guml/infra/GitRepository.groovy
@@ -5,25 +5,33 @@ import org.eclipse.jgit.api.errors.GitAPIException
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.merge.MergeStrategy
 
+import java.nio.file.Files
+
 public class GitRepository {
 
-    private final Repository gitRepo;
+    private final Repository gitRepo
 
     private GitRepository(Repository gitRepo) {
-        this.gitRepo = gitRepo;
+        this.gitRepo = gitRepo
     }
 
     public void refresh() {
         try {
             Git git = new Git(gitRepo);
-            git.pull().setStrategy(MergeStrategy.THEIRS).call();
+            git.pull().setStrategy(MergeStrategy.THEIRS).call()
         } catch (GitAPIException e) {
-            throw new GitException("Cannot pull from remote.", e);
+            throw new GitException("Cannot pull from remote.", e)
         }
     }
 
-    public static GitRepository cloneRemote(String remoteUri, String branch, File localDirectory) {
+    public File getLocalRepository() {
+       gitRepo.directory.parentFile
+    }
+
+    public static GitRepository cloneRemote(String remoteUri, String branch) {
         try {
+            File localDirectory = Files.createTempDirectory("umlrepo").toFile()
+            localDirectory.deleteOnExit()
             Repository gitRepo = Git.cloneRepository()
                     .setURI(remoteUri)
                     .setDirectory(localDirectory)
@@ -31,9 +39,9 @@ public class GitRepository {
                     .setBranch(branch)
                     .call()
                     .getRepository();
-            return new GitRepository(gitRepo);
+            return new GitRepository(gitRepo)
         } catch (IOException | GitAPIException e) {
-            throw new GitException("Cannot clone remote git repository.", e);
+            throw new GitException("Cannot clone remote git repository.", e)
         }
     }
 }
