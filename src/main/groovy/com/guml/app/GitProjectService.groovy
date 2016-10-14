203db7f5cb4ca09f47b3d3acc894c1a349682c83
Leonardo Luz <leoluz@users.noreply.github.com>
2016-10-13 22:52:44 -0400

    Integrate the local git repository with plantuml (#4)

--- src/main/groovy/com/guml/app/GitProjectService.groovy
+++ src/main/groovy/com/guml/app/GitProjectService.groovy
@@ -1,16 +1,15 @@
 package com.guml.app
 
-import com.guml.infra.GitRepository;
+import com.guml.infra.GitRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
 
-import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
+import javax.annotation.PostConstruct
+import java.nio.file.Files
 
 @Service
 @EnableScheduling
@@ -19,16 +18,19 @@ public class GitProjectService {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Value('${git.repo.remote}')
-    private String gitRemoteRepoUrl;
+    private String gitRemoteRepoUrl
 
     @Value('${git.branch}')
-    private String gitBranch;
+    private String gitBranch
+
+    File localDirectory
 
     private GitRepository gitRepository;
 
     @PostConstruct
     private void init() {
-        gitRepository = GitRepository.cloneRemote(gitRemoteRepoUrl, gitBranch);
+        localDirectory = Files.createTempDirectory("umlrepo").toFile();
+        gitRepository = GitRepository.cloneRemote(gitRemoteRepoUrl, gitBranch, localDirectory);
         log.info("Cloned remote repository {}.", gitRemoteRepoUrl);
     }
 
@@ -36,6 +38,18 @@ public class GitProjectService {
     public void updateGitRepo() {
         log.info("Refreshing git repository...");
         gitRepository.refresh();
+    }
+
+    public String getDiagramDsl(String diagramId) {
+        getDiagramFile(diagramId).text
+    }
+
+    private File getDiagramFile(String diagramId) {
+        String diagramPath = "${localDirectory.absolutePath}/dsl/";
+        diagramPath += "${diagramId.replace('-', '/').toLowerCase()}.puml"
+        log.info("Getting dsl from path: ${diagramPath}")
+
+        new File(diagramPath)
     }
 
 }
